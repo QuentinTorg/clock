496bcf3de195e27f8bc4809dc5e0d1cfcc28d664
Sergey Ivanov <powah.serge@gmail.com>
2019-12-29 00:24:40 -0500

    New OO setup minus path planning

--- Clock.ino
+++ Clock.ino
@@ -1,215 +1,200 @@
-// -- motor constants begin
-// x axis
-const int MXSTEP = 6;
-const int MXDIR = 7;
-const int MXEN = 8;
+#include "ClockAPI.h"
+#include <Wire.h>
+#include <RtcDS3231.h>
 
-// y axis
-const int MYSTEP = 11;
-const int MYDIR = 10;
-const int MYEN = 12;
-
-// hour axis
-const int MHSTEP = A2; //A2
-const int MHDIR = A3; //A3
-const int MHEN = A1; //A1
-
-const unsigned char DIR_MINUS = HIGH;
-const unsigned char DIR_PLUS = LOW;
-
-const int MOTOR_POLLING_TIMEOUT = 900;
-// -- motor constants end
-
-// buttons and switches
-const int LSWITCHY = 2;
-const int LSWITCHX = 3;
-const int LSWITCHH = A6;
-const int HOURBUTTON = 5;
-const int MINBUTTON = 4;
-
-// magic
-unsigned MAGIC_GRID_X_OFFSET = 40*8*5 + 40;
-unsigned MAGIC_GRID_Y_OFFSET = 60*8*5 - 20;
-
-// the hour clock ticks from 0 degrees to 180 degrees. This gives us 100 rotations and a total of 800 steps
-unsigned long HOUR_TIME_MILLI_TICK = 21557;
-
-void h_step(unsigned char dir) {
-  dir = dir == HIGH ? LOW : HIGH;
-  Serial.println("in h_step");
-  digitalWrite(MHDIR, dir);
-  digitalWrite(MHSTEP, HIGH);
-  delayMicroseconds(MOTOR_POLLING_TIMEOUT);
-  digitalWrite(MHSTEP, LOW);
-}
-
-void x_step(unsigned char dir) {
- dir = dir == HIGH ? LOW : HIGH;
- digitalWrite(MXDIR, dir);
- digitalWrite(MXSTEP, HIGH);
- delayMicroseconds(MOTOR_POLLING_TIMEOUT);
- digitalWrite(MXSTEP, LOW);
-}
-
-void y_step(unsigned char dir) {
-  digitalWrite(MYDIR, dir);
-  digitalWrite(MYSTEP, HIGH);
-  delayMicroseconds(MOTOR_POLLING_TIMEOUT);
-  digitalWrite(MYSTEP, LOW);
-}
-
-void zeroy() {
-  while (digitalRead(LSWITCHY)) {
-    y_step(DIR_MINUS);
-  }
-}
-
-void zerox() {
-  while (digitalRead(LSWITCHX)) {
-    x_step(DIR_MINUS);
-  }
-}
-
-void zeroh() {
-  Serial.println("zeroing h");
-  while (digitalRead(LSWITCHH)) {
-    h_step(DIR_MINUS);
-  }
-  Serial.println("done zeroing h");
-}
-
-// our path
-const unsigned long grid_x = 8520;
-const unsigned long grid_y = 7408;
-
-const unsigned grid_x_step = grid_x/9;
-const unsigned grid_y_step = grid_y/5;
-
-const unsigned long grid_path_size = grid_x*6;
-const unsigned long grid_time_per_tick = 70;
-
-unsigned long pos_x = 0;
-unsigned long pos_y = 0;
-
-unsigned long g_x = 0;
-unsigned long g_y = 0;
-
-void update_g(unsigned long d)
+struct Time
 {
-  g_x = (d % grid_x) + MAGIC_GRID_X_OFFSET;
-  g_y = d/grid_x*grid_y_step + MAGIC_GRID_Y_OFFSET;
-  Serial.println(g_x);
-}
+    unsigned Hour;
+    unsigned Min;
+    unsigned Sec;
+    unsigned Mil;
 
-void move_towards() {
-  for (int i=0; i<7; ++i)
-    if (pos_x > g_x) {x_step(DIR_MINUS); --pos_x;}
-    
-  if (pos_x < g_x) {x_step(DIR_PLUS);  ++pos_x;}
-  
-  if (pos_y < g_y) {y_step(DIR_PLUS);  ++pos_y;}
-  if (pos_y > g_y) {y_step(DIR_MINUS); --pos_y;}
-}
+    operator<(const Time &o)
+    {
+        return Hour<o.Hour && Min < o.Min &&
+            Sec < o.Sec && Min < o.Mil;
+    }
 
-const unsigned long stepsPerHour = 167;
-const unsigned long totalHSteps = 2000;
-unsigned long h = 0;
-unsigned long h_pos = 0;
-void update_h(unsigned long d)
+    operator>(const Time &o)
+    {
+        return Hour>o.Hour && Min > o.Min &&
+            Sec > o.Sec && Min > o.Mil;
+    }
+
+    operator>=(const Time &o)
+    {
+        return Hour>=o.Hour && Min >= o.Min &&
+            Sec >= o.Sec && Min >= o.Mil;
+    }
+
+    void addMillis(unsigned milliseconds)
+    {
+        Mil += milliseconds;
+        while (Mil >= 1000)
+        {
+            Mil -= 1000;
+            ++Sec;
+            while (Sec >= 60)
+            {
+                ++Min;
+                Sec -= 60;
+                while (Min >= 60)
+                {
+                    Min -= 60;
+                    ++Hour;
+                    while (Hour >= 24)
+                    {
+                        Hour -= 24;
+                    }
+                }
+            }
+        }
+    }
+
+   Time& operator+(const unsigned milliseconds)
+   {
+       addMillis(milliseconds);
+   }
+};
+
+struct TimeRange
 {
-  if (d < stepsPerHour*5)
-    h = d;
-  else
-    h = d + 167;
+    Time begin_;
+    Time end_;
+    TimeRange(Time start, Time stop) : begin_(start),end_(stop)
+    {
+    }
+    bool operator<(const Time &o)
+    {
+        o < begin_;
+    }
+    bool operator>(const Time &o)
+    {
+        o >= end_;
+    }
+};
 
-      Serial.println(h);
-}
-
-void rotate_towards()
+class RTC_Updater
 {
-  if (h_pos < h) { h_step(DIR_PLUS); ++h_pos; }
-  if (h_pos > h) { h_step(DIR_MINUS); --h_pos; }
+private:
+    RtcDS3231<TwoWire> Rtc{Wire};
+    unsigned timer{0};
+    inline static unsigned timeout=6000;
+public:
+    void init(Time &globTime)
+    {
+      Rtc.Begin();
+      Rtc.Enable32kHzPin(false);
+      Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+      timer = timeout;
+
+      RtcDateTime now = Rtc.GetDateTime();
+      globTime.Hour = now.Hour();
+      globTime.Min  = now.Minute();
+      globTime.Sec  = now.Second();
+    }
+    void sync(Time &globTime, unsigned dt)
+    {
+      timer -= dt;
+      if (timer < 0)
+      {
+        RtcDateTime now = Rtc.GetDateTime();
+        globTime.Hour = now.Hour();
+        globTime.Min  = now.Minute();
+        globTime.Sec  = now.Second();
+
+        timer = timeout;
+      }
+    }
+};
+
+// a path is a mapping of time to location
+class Path
+{
+    virtual Point getPos(const Time &t) = 0;
+};
+
+class CirclePath : Path
+{
+private:
+    unsigned R_;
+    unsigned D_;
+public:
+    CirclePath(unsigned Radius, unsigned Duration) : R_(Radius), D_(Duration) {};
+    Point getPos(const Time &t)
+    {
+        // do magic
+    };
+};
+
+class SquarePath : Path
+{
+    unsigned S_;
+    unsigned D_;
+public:
+    SquarePath(unsigned Size, unsigned Duration) : S_(Size), D_(Duration) {};
+    // do magic
+    Point getPos(const Time &t)
+    {
+    };
+};
+
+class BoardPathMapper : Path
+{
+private:
+    inline static Point boardOffset_{1600,2400};
+    inline static Point minuteSquareOffset_{946,1480};
+    SquarePath minutePath{400,1000};
+public:
+    Point getPos(const Time &t)
+    {
+       return minuteSquareOffset_ + minutePath.getPos(t);
+    };
+};
+
+class HourPosMapper
+{
+public:
+    pos_t getPos(const Time &t)
+    {
+        // some spiffy function for the arm
+    };
+};
+
+
+// -- MAIN LOGIC BEGIN
+
+
+RTC_Updater rtcClock_;
+Time globTime_{0,0,0,0};
+BoardPathMapper pathMapper_;
+HourPosMapper hourPosMapper_;
+
+Gantry gantry_;
+HourHand hourHand_;
+
+void setup()
+{
+    initClockPins();
+    rtcClock_.init(globTime_);    
+    gantry_.zero();
+    hourHand_.zero();
 }
 
-void setup() {
-  // Sets the two pins as Outputs
-  Serial.begin(9600);
-
-  // x motor setup
-  pinMode(MXSTEP, OUTPUT);
-  pinMode(MXDIR, OUTPUT);
-  pinMode(MXEN, OUTPUT);
-
-  digitalWrite(MXEN, LOW);
-
-  // y motor setup
-  pinMode(MYSTEP, OUTPUT);
-  pinMode(MYDIR, OUTPUT);
-  pinMode(MYEN, OUTPUT);
-
-  digitalWrite(MYEN, LOW);
-
-  // clock setup
-  pinMode(MHSTEP, OUTPUT);
-  pinMode(MHDIR, OUTPUT);
-  pinMode(MHEN, OUTPUT);
-
-  digitalWrite(MHEN, LOW);
-
-  // zeroing switch setup
-  pinMode(LSWITCHY, INPUT_PULLUP);
-  pinMode(LSWITCHX, INPUT_PULLUP);
-  pinMode(LSWITCHH, INPUT_PULLUP);
-  pinMode(HOURBUTTON, INPUT_PULLUP);
-  pinMode(MINBUTTON, INPUT_PULLUP);
-
-  zerox();
-  zeroy();
-  //zeroh();
-  pos_x = pos_y = h_pos = 0;
-}
-
-unsigned long dist = 0;
-unsigned long h_dist = 0;
-unsigned long distTimeCtr = 0;
-unsigned long hourTimeCtr = 0;
-unsigned long time = 0;
-unsigned long lastTime = 0;
+unsigned long time{0}, lastTime{0};
 void loop()
 {
-  time = millis();
-  if (lastTime == 0) lastTime = time;
-  unsigned dt = time - lastTime;
-  lastTime = time;
-  
-  distTimeCtr += dt;
-  hourTimeCtr += dt;
+    time = millis();
+    if (lastTime == 0) lastTime = time;
+    unsigned dt = time - lastTime;
+    lastTime = time;
 
-  // hour handling
-  if (digitalRead(HOURBUTTON) == LOW)
-    ++h_dist;
-    
-  if (hourTimeCtr >= HOUR_TIME_MILLI_TICK)
-  {
-    ++h_dist;
-    hourTimeCtr -= HOUR_TIME_MILLI_TICK;
-  }
+    globTime_.addMillis(dt);
+    rtcClock_.sync(globTime_, dt);
 
-  // normal handling of distance increment
-  if (distTimeCtr >= grid_time_per_tick/10)
-  {
-    dist++;
-    distTimeCtr -= grid_time_per_tick/10;
-  }
+    auto gpos = pathMaper_.getPos(globTime_);
+    auto hpos = ourPosMapper(globTime_);
 
- if (digitalRead(MINBUTTON) == LOW)
-   dist++;
-
-  if (dist >= grid_path_size) dist -= grid_path_size;
-
-  // updating of gantry towards global position
-  update_g(dist);
-  update_h(h_dist);
-  move_towards(); //move motors towards minute place
-  rotate_towards();
+    gantry_.moveTo(gpos);
+    hourHand_.moveTo(hpos);
 }
---
+++ ClockAPI.h
@@ -0,0 +1,174 @@
+// x axis pins
+const int MXSTEP = 3;
+const int MXDIR = 2;
+
+// y axis pins
+const int MYSTEP = 5;
+const int MYDIR = 4;
+
+// hour axis pins
+const int MHSTEP = A2; //A2
+const int MHDIR = A3; //A3
+
+// motor directions
+const unsigned char DIR_MINUS = HIGH;
+const unsigned char DIR_PLUS = LOW;
+
+// buttons and switches
+const int LSWITCHY = 10;
+const int LSWITCHX = 9;
+const int LSWITCHH = 8;
+
+const int BUTTON1 = A3;
+const int BUTTON2 = A6;
+const int BUTTON3 = A7;
+
+// initializes motor pins - must be called before any of the following functions
+void initClockPins()
+{
+  // x motor setup
+  pinMode(MXSTEP, OUTPUT);
+  pinMode(MXDIR, OUTPUT);
+
+  // y motor setup
+  pinMode(MYSTEP, OUTPUT);
+  pinMode(MYDIR, OUTPUT);
+
+  // zeroing switch setup
+  pinMode(LSWITCHY, INPUT_PULLUP);
+  pinMode(LSWITCHX, INPUT_PULLUP);
+  pinMode(LSWITCHH, INPUT_PULLUP);
+
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
+  pinMode(BUTTON3, INPUT_PULLUP);
+}
+
+constexpr unsigned MOTOR_POLLING_TIMEOUT = 900;
+// step the hour hand in a direction
+void h_step(unsigned char dir)
+{
+  dir = dir == HIGH ? LOW : HIGH;
+  Serial.println("in h_step");
+  digitalWrite(MHDIR, dir);
+  digitalWrite(MHSTEP, HIGH);
+  delayMicroseconds(MOTOR_POLLING_TIMEOUT);
+  digitalWrite(MHSTEP, LOW);
+}
+
+// step the gantry x-axis in a direction
+void x_step(unsigned char dir)
+{
+ dir = dir == HIGH ? LOW : HIGH;
+ digitalWrite(MXDIR, dir);
+ digitalWrite(MXSTEP, HIGH);
+ delayMicroseconds(MOTOR_POLLING_TIMEOUT);
+ digitalWrite(MXSTEP, LOW);
+}
+
+// step the gantry y-axis in a direction
+void y_step(unsigned char dir)
+{
+  digitalWrite(MYDIR, dir);
+  digitalWrite(MYSTEP, HIGH);
+  delayMicroseconds(MOTOR_POLLING_TIMEOUT);
+  digitalWrite(MYSTEP, LOW);
+}
+
+// zero the y
+void zeroy()
+{
+  while (digitalRead(LSWITCHY))
+  {
+    y_step(DIR_MINUS);
+  }
+}
+
+void zerox()
+{
+  while (digitalRead(LSWITCHX)) {
+    x_step(DIR_MINUS);
+  }
+}
+
+void zeroh()
+{
+  Serial.println("zeroing h");
+  while (digitalRead(LSWITCHH)) {
+    h_step(DIR_MINUS);
+  }
+  Serial.println("done zeroing h");
+}
+
+// higher level abstractions
+using pos_t=unsigned long;
+
+struct Point
+{
+    pos_t x,y;
+};
+
+class Gantry
+{
+private:
+    Point dst_ {0,0};
+    Point pos_ {0,0};
+    Point prev_ {0,0};
+    unsigned long delta;
+public:
+    void zero()
+    {
+        zerox();
+        zeroy();
+        pos_ = {0,0};
+    }
+
+    void moveTo(const Point &pt)
+    {
+    }
+
+    void update(unsigned dt)
+    {
+    }
+};
+
+class HourHand
+{
+private:
+    pos_t pos_ {0}; // model of where we're supposed to be
+    pos_t dst_ {0}; // actual cursor location
+    unsigned long delta {0};
+public:
+    void zero()
+    {
+        zeroh();
+    }
+
+    void moveTo(pos_t dst)
+    {
+    }
+
+    void update(unsigned dt)
+    {
+    }
+};
+
+//unsafe and janky
+template<typename T, unsigned S>
+class FixedVec
+{
+private:
+    T dat_[S];
+    unsigned size_{0};
+public:
+    void push_back(const T& o)
+    {
+        dat_[size_] = o;
+        ++size_;
+    };
+
+    T& operator[](unsigned i)
+    {
+        return dat_[i];
+    }
+};
